d281ee3da6f7f5a033551d4a0bfc5b28debde746
siangting <siangting200@gmail.com>
2026-03-31 11:59:00 +0800

    feat: change micro ros agent version to humble

--- run_agent.sh
+++ run_agent.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --net=host --privileged   -v /dev:/dev   microros/micro-ros-agent:jazzy   serial --dev /dev/ttyACM0 -b 115200 -v6
+docker run -it --rm --net=host --privileged   -v /dev:/dev   microros/micro-ros-agent:humble   serial --dev /dev/ttyACM0 -b 115200 -v6
